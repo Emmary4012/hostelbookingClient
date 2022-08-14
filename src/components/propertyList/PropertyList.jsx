e657adb9dc252f6108ca57b3b10e9d1fc14e2683
Emmanuel Apita <97722740+Emmary4012@users.noreply.github.com>
2022-08-14 16:25:59 +0300

    Update PropertyList.jsx

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -4,7 +4,7 @@ import useFetch from "../../hooks/useFetch";
 
 const PropertyList = () => {
 
-  const {data, loading, error} = useFetch("/api/hotels/countByType")
+  const {data, loading, error} = useFetch("https://hostel7booking.herokuapp.com/api/hotels/countByType")
   const images = [
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
