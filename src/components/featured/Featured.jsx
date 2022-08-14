c63f657b90befb232fddd6fd40eaaaac6d2e2ca2
Emmanuel Apita <97722740+Emmary4012@users.noreply.github.com>
2022-08-14 16:24:31 +0300

    Update Featured.jsx

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 //import axios from "axios";
 
 const Featured = () => {
-  const {data, loading, error} = useFetch("/api/hotels/countByCity?cities=berlin,madrid,london")
+  const {data, loading, error} = useFetch("https://hostel7booking.herokuapp.com/api/hotels/countByCity?cities=berlin,madrid,london")
 
   return (
     <div className="featured">
